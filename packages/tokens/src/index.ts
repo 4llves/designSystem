6243526ed8f7ed4dcd1e2e5c0e036a3447fe8e6b
4llves <jhonatab4@gmail.com>
2024-08-19 23:38:45 -0300

    feat: configure tokens

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,1 +1,11 @@
 export * from './colors';
+
+
+export * from './radii';
+export * from './space';
+
+export * from './font-sizes';
+export * from './font-weights';
+export * from './fonts';
+export * from './line-heights';
+
